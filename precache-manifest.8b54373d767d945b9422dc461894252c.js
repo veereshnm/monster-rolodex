ac5e740ae35df043dc7c021180579b71b7cab7c4
Veeresh Nerlige <veeresh.nerlige@insperity.com>
2019-11-02 21:29:37 -0500

    Updates

---
+++ precache-manifest.8b54373d767d945b9422dc461894252c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd1fbf4869fbc00f356a07e3cfabff7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3a6801fb6c736a2abbc5",
+    "url": "/monster-rolodex/static/css/main.a9e98b59.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monster-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "3a6801fb6c736a2abbc5",
+    "url": "/monster-rolodex/static/js/main.345a45e4.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
