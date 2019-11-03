ecf5dd102e054b63c388bf2d4abbd56d82bd6dea
Veeresh Nerlige <veeresh.nerlige@insperity.com>
2019-11-02 21:33:39 -0500

    Adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) => {
+    return <div className="card-container">
+    <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="robo" />
+    <h2 key={props.monster.id}>{props.monster.name}</h2>
+    <p> { props.monster.email }</p>
+    </div>
+}
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search-box.styles.css'
+
+export const SearchBox = ( {placeholder, handleChange } ) => {
+    return <input 
+    type="search"
+    className="search"
+    onChange= {handleChange} 
+    placeholder= { placeholder }/>
+}
